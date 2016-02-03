b7efeac67c4a6f81340afd0d47251d1fc0328e3a
shellrock <hanwoody@gmail.com>
2016-02-03 18:38:16 +0800

    add hello.jar

--- test.sh
+++ test.sh
@@ -5,8 +5,7 @@ set -xe
 docker rm -f "$CON_NAME" > /dev/null 2>&1 || true
 docker run -d --name $CON_NAME $IMAGE sleep 1000
 
-docker cp Hello.class $CON_NAME:/app/
-docker exec $CON_NAME jar cvf hello.jar Hello.class
+docker cp hello.jar $CON_NAME:/app/
 docker exec $CON_NAME java hello.jar|grep -i "hello"
 
 docker rm -f $CON_NAME

cdbb2153063dbef4b0e8e12b72b627b5e2c59e34
shellrock <hanwoody@gmail.com>
2016-02-03 15:09:21 +0800

    fix test

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ docker run -d --name $CON_NAME $IMAGE sleep 1000
 
 if [ ! -f /tmp/helloworld.jar ]; then
   wget -O /tmp/helloworld.jar.zip http://www.java2s.com/Code/JarDownload/helloworld/helloworld.jar.zip
-  unzip helloworld.jar.zip
+  unzip /tmp/helloworld.jar.zip
 fi
 
 docker cp /tmp/helloworld.jar $CON_NAME:/app/

025e13bc03bc3bd2c555273c4f210b3e8034d601
shellrock <hanwoody@gmail.com>
2016-02-03 18:55:10 +0800

    fix test

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ docker rm -f "$CON_NAME" > /dev/null 2>&1 || true
 docker run -d --name $CON_NAME $IMAGE sleep 1000
 
 docker cp Hello.class $CON_NAME:/app/
-docker exec $CON_NAME java /app/Hello|grep -i "hello"
+docker exec $CON_NAME java Hello|grep -i "hello"
 
 docker rm -f $CON_NAME
 
